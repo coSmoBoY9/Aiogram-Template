f6e51963c5b40e1de9de2b6d9b7dcd61a31f5e43
coSmoBoY9 <bekhruzallaberganov@gmail.com>
2022-03-05 17:03:10 +0500

    fix bugs

--- data/config.py
+++ data/config.py
@@ -5,5 +5,5 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+BOT_TOKEN = "5166462658:AAFpOziZ1bP0L1iKeQWqJ05LGHOZuTq3dB4" # Bot toekn
+ADMINS = [5095576858] # adminlar ro'yxati
